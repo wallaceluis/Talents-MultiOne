bf748f1218e0558b04c792e042ab1d4e6a2c7ba0
Wallace Luis <wallaceluis144@gmail.com>
2025-10-17 09:30:28 -0300

    Adicionado export default a todos os componentes principais

---
+++ frontend/app/dashboard/layout.tsx
@@ -0,0 +1,14 @@
+import "../styles/globals.css";
+import { ReactNode } from "react";
+
+export default function DashbordLayout({ children }: { children: ReactNode }) {
+  return (
+    <html lang="pt-BR">
+      <body className="flex min-h-screen bg-gray-100">
+        <div className="flex-1 flex flex-col">
+          <main className="p-6">{children}</main>
+        </div>
+      </body>
+    </html>
+  );
+}
